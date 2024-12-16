3296e36fc83050f55d0e67588ddf5310c8fe090c
李家进 <36221557+ML666666@users.noreply.github.com>
2024-12-16 14:50:23 +0800

    feat: change config for the navbar and sidebar

--- src/.vuepress/navbar/en.ts
+++ src/.vuepress/navbar/en.ts
@@ -1,47 +1,33 @@
 import { navbar } from "vuepress-theme-hope";
 
 export const enNavbar = navbar([
-  "/",
-  "/node/",
-  {
-    text: "Posts",
-    icon: "pen-to-square",
-    prefix: "/posts/",
-    children: [
-      {
-        text: "Apple",
-        icon: "pen-to-square",
-        prefix: "apple/",
-        children: [
-          { text: "Apple1", icon: "pen-to-square", link: "1" },
-          { text: "Apple2", icon: "pen-to-square", link: "2" },
-          "3",
-          "4",
-        ],
-      },
-      {
-        text: "Banana",
-        icon: "pen-to-square",
-        prefix: "banana/",
-        children: [
-          {
-            text: "Banana 1",
-            icon: "pen-to-square",
-            link: "1",
-          },
-          {
-            text: "Banana 2",
-            icon: "pen-to-square",
-            link: "2",
-          },
-          "3",
-          "4",
-        ],
-      },
-      { text: "Cherry", icon: "pen-to-square", link: "cherry" },
-      { text: "Dragon Fruit", icon: "pen-to-square", link: "dragonfruit" },
-      "tomato",
-      "strawberry",
-    ],
-  },
-]);
+    {
+      text: "主页",
+      link: "/",
+      icon: "home",
+    },
+    {
+      text: "node",
+      link: "/node/",
+      icon: "laptop-code",
+      activeMatch: "^/node",
+    },
+    {
+      text: "设计模式",
+      link: "/patterns/",
+      icon: "code",
+      activeMatch: "^/patterns",
+    },
+    {
+      text: "测试",
+      link: "/softwareTesting/",
+      icon: "spell-check",
+      activeMatch: "^/softwareTesting",
+    },
+    {
+      text: "杂项",
+      link: "/someThing/",
+      icon: "layer-group",
+      activeMatch: "^/someThing",
+    },
+  ]);
